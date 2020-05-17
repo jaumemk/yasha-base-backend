7abb505260b91f9d0da6b2bda4ab3fc74b30f123
Jaume MK <jaumemk@gmail.com>
2020-05-17 20:36:28 +0200

    page manager translations

---
+++ resources/lang/es/pagemanager.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'change_template_confirmation' => '¿Seguro que desea canviar la disposición? Podría perder elementos ya editados.',
+    'content' => 'Contenido',
+    'content_placeholder' => 'Su contenido aquí',
+    'meta_description' => 'Meta Description',
+    'meta_keywords' => 'Meta Keywords',
+    'meta_title' => 'Meta Title',
+    'metas' => 'Metas',
+    'name' => 'Nombre',
+    'open' => 'Abrir',
+    'page' => 'página',
+    'page_name' => 'Nombre de página (para administradores)',
+    'page_slug' => 'Slug (URL)',
+    'page_slug_hint' => 'Se generará automáticamente a partir del título si se encuentra vacío',
+    'page_title' => 'Título de página',
+    'pages' => 'páginas',
+    'slug' => 'Slug',
+    'template' => 'Disposición',
+    'template_not_found' => 'No se encuentra la disposición de esta página. Contacte con el adminstrador.',
+];
